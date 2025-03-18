e2acecdc85f4062b5254cfe53375848f546460b3
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2025-03-18 17:19:53 +0300

    Update scenario hook tests

--- src/test/java/com/epam/reportportal/cucumber/HooksTest.java
+++ src/test/java/com/epam/reportportal/cucumber/HooksTest.java
@@ -18,16 +18,20 @@ package com.epam.reportportal.cucumber;
 
 import com.epam.reportportal.cucumber.integration.TestScenarioReporter;
 import com.epam.reportportal.cucumber.integration.util.TestUtils;
+import com.epam.reportportal.listeners.ItemStatus;
 import com.epam.reportportal.listeners.ListenerParameters;
 import com.epam.reportportal.service.ReportPortal;
 import com.epam.reportportal.service.ReportPortalClient;
 import com.epam.reportportal.util.test.CommonUtils;
+import com.epam.ta.reportportal.ws.model.FinishTestItemRQ;
+import com.epam.ta.reportportal.ws.model.StartTestItemRQ;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -35,10 +39,12 @@ import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.*;
 
 public class HooksTest {
-
 	@CucumberOptions(features = "src/test/resources/features/DummyScenario.feature", glue = {
 			"com.epam.reportportal.cucumber.integration.hooks.step" }, plugin = {
 			"com.epam.reportportal.cucumber.integration.TestScenarioReporter" })
@@ -78,11 +84,9 @@ public class HooksTest {
 	private final String suiteId = CommonUtils.namedId("feature_");
 	private final String scenarioId = CommonUtils.namedId("scenario_");
 	private final List<String> stepIds = Stream.generate(() -> CommonUtils.namedId("step_")).limit(4).collect(Collectors.toList());
-
 	private final List<Pair<String, String>> nestedSteps = stepIds.stream()
 			.flatMap(s -> Stream.generate(() -> CommonUtils.namedId("nested_step_")).limit(2).map(ns -> Pair.of(s, ns)))
 			.collect(Collectors.toList());
-
 	private final ListenerParameters params = TestUtils.standardParameters();
 	private final ReportPortalClient client = mock(ReportPortalClient.class);
 	private final ExecutorService executorService = Executors.newSingleThreadExecutor();
@@ -121,10 +125,34 @@ public class HooksTest {
 
 		verify(client, times(1)).startTestItem(any());
 		verify(client, times(1)).startTestItem(same(suiteId), any());
-		verify(client, times(4)).startTestItem(same(scenarioId), any());
-		verify(client, times(1)).startTestItem(same(stepIds.get(0)), any());
-		verify(client, times(1)).startTestItem(same(stepIds.get(3)), any());
+		ArgumentCaptor<StartTestItemRQ> stepCaptor = ArgumentCaptor.forClass(StartTestItemRQ.class);
+		verify(client, times(4)).startTestItem(same(scenarioId), stepCaptor.capture());
+		ArgumentCaptor<StartTestItemRQ> beforeScenarioCaptor = ArgumentCaptor.forClass(StartTestItemRQ.class);
+		verify(client, times(1)).startTestItem(same(stepIds.get(0)), beforeScenarioCaptor.capture());
+		ArgumentCaptor<StartTestItemRQ> afterScenarioCaptor = ArgumentCaptor.forClass(StartTestItemRQ.class);
+		verify(client, times(1)).startTestItem(same(stepIds.get(3)), afterScenarioCaptor.capture());
 		verify(client, times(6)).log(any(List.class));
+
+		List<StartTestItemRQ> steps = stepCaptor.getAllValues();
+		assertThat(steps.get(0).getName(), equalTo("Before hooks"));
+		assertThat(steps.get(steps.size() - 1).getName(), equalTo("After hooks"));
+
+		StartTestItemRQ beforeStep = beforeScenarioCaptor.getValue();
+		assertThat(
+				beforeStep.getName(),
+				equalTo("com.epam.reportportal.cucumber.integration.hooks.scenario.one.EmptySteps.my_before_hook()")
+		);
+
+		StartTestItemRQ afterStep = afterScenarioCaptor.getValue();
+		assertThat(
+				afterStep.getName(),
+				equalTo("com.epam.reportportal.cucumber.integration.hooks.scenario.one.EmptySteps.my_after_hook()")
+		);
+
+		ArgumentCaptor<FinishTestItemRQ> finishCaptor = ArgumentCaptor.forClass(FinishTestItemRQ.class);
+		verify(client, times(8)).finishTestItem(anyString(), finishCaptor.capture());
+		List<FinishTestItemRQ> finishSteps = finishCaptor.getAllValues();
+		finishSteps.subList(0, finishSteps.size() - 2).forEach(step -> assertThat(step.getStatus(), equalTo(ItemStatus.PASSED.name())));
 	}
 
 	@Test
@@ -134,10 +162,40 @@ public class HooksTest {
 
 		verify(client, times(1)).startTestItem(any());
 		verify(client, times(1)).startTestItem(same(suiteId), any());
-		verify(client, times(4)).startTestItem(same(scenarioId), any());
-		verify(client, times(2)).startTestItem(same(stepIds.get(0)), any());
-		verify(client, times(2)).startTestItem(same(stepIds.get(3)), any());
+		ArgumentCaptor<StartTestItemRQ> stepCaptor = ArgumentCaptor.forClass(StartTestItemRQ.class);
+		verify(client, times(4)).startTestItem(same(scenarioId), stepCaptor.capture());
+		ArgumentCaptor<StartTestItemRQ> beforeScenarioCaptor = ArgumentCaptor.forClass(StartTestItemRQ.class);
+		verify(client, times(2)).startTestItem(same(stepIds.get(0)), beforeScenarioCaptor.capture());
+		ArgumentCaptor<StartTestItemRQ> afterScenarioCaptor = ArgumentCaptor.forClass(StartTestItemRQ.class);
+		verify(client, times(2)).startTestItem(same(stepIds.get(3)), afterScenarioCaptor.capture());
 		verify(client, times(10)).log(any(List.class));
+
+		List<StartTestItemRQ> steps = stepCaptor.getAllValues();
+		assertThat(steps.get(0).getName(), equalTo("Before hooks"));
+		assertThat(steps.get(steps.size() - 1).getName(), equalTo("After hooks"));
+
+		List<StartTestItemRQ> beforeSteps = beforeScenarioCaptor.getAllValues();
+		beforeSteps.forEach(beforeStep -> assertThat(
+				beforeStep.getName(),
+				anyOf(
+						equalTo("com.epam.reportportal.cucumber.integration.hooks.scenario.two.EmptySteps.my_first_before_hook()"),
+						equalTo("com.epam.reportportal.cucumber.integration.hooks.scenario.two.EmptySteps.my_second_before_hook()")
+				)
+		));
+
+		List<StartTestItemRQ> afterSteps = afterScenarioCaptor.getAllValues();
+		afterSteps.forEach(afterStep -> assertThat(
+				afterStep.getName(),
+				anyOf(
+						equalTo("com.epam.reportportal.cucumber.integration.hooks.scenario.two.EmptySteps.my_first_after_hook()"),
+						equalTo("com.epam.reportportal.cucumber.integration.hooks.scenario.two.EmptySteps.my_second_after_hook()")
+				)
+		));
+
+		ArgumentCaptor<FinishTestItemRQ> finishCaptor = ArgumentCaptor.forClass(FinishTestItemRQ.class);
+		verify(client, times(10)).finishTestItem(anyString(), finishCaptor.capture());
+		List<FinishTestItemRQ> finishSteps = finishCaptor.getAllValues();
+		finishSteps.subList(0, finishSteps.size() - 2).forEach(step -> assertThat(step.getStatus(), equalTo(ItemStatus.PASSED.name())));
 	}
 
 	@Test
